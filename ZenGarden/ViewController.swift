95cb0a7a4e36c976cf40a1b01884512cb567fbd5
edmund-h <447ght@gmail.com>
2017-02-28 17:50:31 -0500

    rake man rake drags things around and bush stays in one place

--- ZenGarden/ViewController.swift
+++ ZenGarden/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var swordInRock1: UIImageView!
+    @IBOutlet weak var shrub1: UIImageView!
+    @IBOutlet weak var rockManRock: UIImageView!
+    @IBOutlet weak var rakeManRake: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        swordInRock1.isUserInteractionEnabled = true
+        shrub1.isUserInteractionEnabled = true
+        rockManRock.isUserInteractionEnabled = true
+        rakeManRake.isUserInteractionEnabled = true
+        
+        let items = [swordInRock1, shrub1, rockManRock, rakeManRake]
+        items.forEach({
+            let dragger = UIPanGestureRecognizer(target: self, action: #selector (dragView))
+            ($0!).addGestureRecognizer(dragger)
+            print ("added!")
+        })
+        
+            
+        
     }
-
+    
+    
+   
+    func dragView (_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        sender.view!.center = point
+        print (point)
+    }
+    
 }
 
