bf6c477fe0eb57cffc2d7eeda41a7b29212c41ec
edmund-h <447ght@gmail.com>
2017-02-28 22:55:01 -0500

    forgot that we had to implement win conditions

--- ZenGarden/ViewController.swift
+++ ZenGarden/ViewController.swift
@@ -7,29 +7,29 @@
 //
 
 import UIKit
+import Darwin
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
-    @IBOutlet weak var swordInRock1: UIImageView!
-    @IBOutlet weak var shrub1: UIImageView!
+    @IBOutlet weak var kingBoySword: UIImageView!
+    @IBOutlet weak var kingBoyShrub: UIImageView!
     @IBOutlet weak var rockManRock: UIImageView!
     @IBOutlet weak var rakeManRake: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        swordInRock1.isUserInteractionEnabled = true
-        shrub1.isUserInteractionEnabled = true
+        kingBoySword.isUserInteractionEnabled = true
+        kingBoyShrub.isUserInteractionEnabled = true
         rockManRock.isUserInteractionEnabled = true
         rakeManRake.isUserInteractionEnabled = true
         
-        let items = [swordInRock1, shrub1, rockManRock, rakeManRake]
+        let items = [kingBoySword, kingBoyShrub, rockManRock, rakeManRake]
         items.forEach({
             let dragger = UIPanGestureRecognizer(target: self, action: #selector (dragView))
             ($0!).addGestureRecognizer(dragger)
             print ("added!")
         })
         
-            
         
     }
     
@@ -39,7 +39,41 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         let point = sender.location(in: view)
         sender.view!.center = point
         print (point)
+        
+        areAncientsJustified()
     }
     
+    func areAncientsJustified (){
+        
+        let items = [kingBoySword, kingBoyShrub, rockManRock, rakeManRake]
+        let rockManRockAxis = 370-rockManRock.center.y
+        let kingBoySwordAxis = 370-kingBoySword.center.y
+        let shrubrake = distance(kingBoyShrub.center, rakeManRake.center)
+        guard (kingBoySword.center.x < 80 && kingBoySword.center.y < 80) ||
+            (kingBoySword.center.x > 310 && kingBoySword.center.y > 690) else {return}
+        guard shrubrake < 100 else {return}
+        guard (rockManRockAxis / -1 * (rockManRockAxis)) !=
+            (kingBoySwordAxis / -1 * (kingBoySwordAxis)) else {return}
+        
+        print ("yay")
+        let alertController = UIAlertController(title: "ANCIENTS ARE JUSTIFIED", message: "MU MU", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "MU MU", style: .default) { (action:UIAlertAction) in
+            print("Now scramble");
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion:{
+            for item in items {
+                item!.center.x = CGFloat(arc4random_uniform(350))
+                item!.center.y = CGFloat(arc4random_uniform(750))
+            }
+        })
+        
+    }
+    
+    func distance (_ p1: CGPoint, _ p2:CGPoint)->Float{
+        let distance = hypotf(Float(p1.x - p2.x), Float(p1.y - p2.y))
+        return distance
+    }
 }
+
 
